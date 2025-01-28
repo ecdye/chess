b40a073f36e112ab160a1b3502faa39d818be783
Ethan Dye <mrtops03@gmail.com>
2025-01-28 14:32:06 -0700

    Implement isInStalemate
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -191,7 +191,17 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        for (int row = 1; row <= 8; row++) {
+            for (int col = 1; col <= 8; col++) {
+                ChessPosition position = new ChessPosition(row, col);
+                ChessPiece piece = gameBoard.getPiece(position);
+                if (piece != null && piece.getTeamColor() == teamColor) {
+                    if (!validMoves(position).isEmpty()) return false;
+                }
+            }
+        }
+        if (isInCheck(teamColor)) return false;
+        return true;
     }
 
     /**
