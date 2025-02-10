9c5dc226dc193893c59e81f7af1cba8eab19e04c
Ethan Dye <mrtops03@gmail.com>
2025-02-10 14:20:06 -0700

    Implement AuthDAO
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,5 +1,10 @@
 package dataaccess;
 
-public interface AuthDAO {
-    
+import model.UserData;
+import model.AuthData;
+
+interface AuthDAO {
+    AuthData createAuth(UserData userData);
+    AuthData getAuth(String authToken);
+    Boolean deleteAuth(String authToken);
 }
---
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,33 @@
+package dataaccess;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import model.AuthData;
+import model.UserData;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private HashMap<String, AuthData> authDataMap = new HashMap<>();
+
+    public MemoryAuthDAO() {}
+
+    @Override
+    public AuthData createAuth(UserData userData) {
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, userData.username());
+        authDataMap.put(authToken, authData);
+
+        return authData;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return authDataMap.get(authToken);
+    }
+
+    @Override
+    public Boolean deleteAuth(String authToken) {
+        AuthData authData = authDataMap.remove(authToken);
+        return authData != null;
+    }
+}
