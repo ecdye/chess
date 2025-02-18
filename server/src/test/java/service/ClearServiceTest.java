a17176d533393b1cb951876ac9c4529b255e8dbc
Ethan Dye <mrtops03@gmail.com>
2025-02-18 10:20:08 -0700

    Add unit tests for ClearService
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

---
+++ server/src/test/java/service/ClearServiceTest.java
@@ -0,0 +1,32 @@
+package service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.ClearResult;
+import model.UserData;
+
+public class ClearServiceTest {
+    @Test
+    void testClearDatabase() throws DataAccessException {
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        userDAO.createUser(new UserData("Test", "password", "test@example.com"));
+
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        AuthData authData = authDAO.createAuth(new UserData("Test", "password", "test@example.com"));
+
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
+        int gameID = gameDAO.createGame("Best Game!");
+
+        ClearService clearService = new ClearService(authDAO, userDAO, gameDAO);
+        Assertions.assertEquals(clearService.clearDatabase(), new ClearResult(null));
+        Assertions.assertNull(userDAO.getUser("Test"));
+        Assertions.assertNull(authDAO.getAuth(authData.authToken()));
+        Assertions.assertNull(gameDAO.getGame(gameID));
+    }
+}
